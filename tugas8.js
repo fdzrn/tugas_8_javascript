95670a49a3c964fab8fef0acc205d6f31df1ab05
fdzrn <fadzriangibran@gmail.com>
2019-07-31 14:54:03 +0700

    Tugas 8
    tugas ini saya kirimkan untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function tugasDepalan(argument) {
+	let dataObject = {
+		nama: 'Fadjrian',
+		umur: 23,
+		tinggi: 172,
+		alamat: 'Sukabumi'
+	}
+	console.log(dataObject);
+	console.log("");
+	dataObject.alamat = 'Bandung';
+	dataObject.pria = true;
+	console.log(dataObject);
+}
+tugasDepalan();
